051014a84e3819fa6d3046b910490069512724a7
stumpfeklinge <136319104+stumpfeklinge@users.noreply.github.com>
2023-11-10 14:36:57 +0300

    Update testRGB.py

--- testRGB.py
+++ testRGB.py
@@ -1,4 +1,3 @@
-
 from keras.datasets import cifar10
 from keras.src.constraints import maxnorm
 from tensorflow import keras
@@ -6,6 +5,7 @@ from Conv2DLayer import Conv2DLayer
 from MaxPool2DLayer import MaxPool2DLayer
 from Flatten2D import Flatten2D
 from Dense2D import Dense2D
+from Dropout2DLayer import Dropout
 
 
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -23,11 +23,25 @@ class_num = y_test.shape[1]
 model = keras.Sequential([
     Conv2DLayer(32, (3, 3), input_shape=x_train.shape[1:], activation='relu', padding='same'),
     MaxPool2DLayer((2, 2)),
-    Conv2DLayer(64, (3, 3), padding='same', activation='relu'),
+    Dropout(0.1),
+
+    Conv2DLayer(64, (3, 3), input_shape=x_train.shape[1:], activation='relu', padding='same'),
     MaxPool2DLayer((2, 2)),
+    Dropout(0.1),
+
+    Conv2DLayer(128, (3, 3), padding='same', activation='relu'),
+    MaxPool2DLayer((2, 2)),
+    Dropout(0.1),
+
+    Conv2DLayer(256, (3, 3), padding='same', activation='relu'),
+    MaxPool2DLayer((2, 2)),
+    Dropout(0.1),
+
     Flatten2D(),
-    Dense2D(256, kernel_constraint=maxnorm(3), activation='relu'),
     Dense2D(128, kernel_constraint=maxnorm(3), activation='relu'),
+    Dropout(0.1),
+    Dense2D(256, kernel_constraint=maxnorm(3), activation='relu'),
+    Dropout(0.1),
     Dense2D(class_num,  activation='softmax')
 ])
 
